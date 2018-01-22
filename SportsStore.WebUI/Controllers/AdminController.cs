1100a8c3f28c7606385ff2f8be30d58d58547eb4
DedalJun <34416971+DedalJun@users.noreply.github.com>
2018-01-22 14:53:29 +0200

    Add files via upload

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.WebUI.Infrastructure.Filters;
+
+namespace SportsStore.WebUI.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        [Profile]
+        [ProfileResult]
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+        //[Bind(Exclude = "ProductID")]
+        [HttpPost]
+        public ActionResult Edit(Product product, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
+
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        public ActionResult Delete(int productId)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (prod != null)
+            {
+                repository.DeleteProduct(prod);
+                TempData["message"] = string.Format("{0} was deleted", prod.Name);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/Filters/ProfileResultAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace SportsStore.WebUI.Infrastructure.Filters
+{
+    public class ProfileResultAttribute: FilterAttribute, IResultFilter
+    {
+        private Stopwatch timer;
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterConttext)
+        {
+            timer.Stop();
+            filterConttext.HttpContext.Response.Write(string.Format("Result execution - elapsed time: {0}",
+                timer.Elapsed.TotalSeconds));
+            timer.Reset();
+        }
+    }
+}
